7a1725d76f6c82f6ac2a252e7916877c8e14f767
ShadaabIqbal <iqbalshadaab@gmail.com>
2023-04-17 06:17:41 +0530

    DONE

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const route = require('./src/route/route')
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://ShadaabIqbal:tX5n1spRVfR4YJOc@mycluster.cuj3crc.mongodb.net/ShadaabIqbal-DB?retryWrites=true&w=majority', { useNewUrlParser: true }, mongoose.set('strictQuery', false))
+    .then(function () { console.log('mongoDB is connected') })
+    .catch(function (error) { console.log(error) })
+
+app.use('/', route)
+
+app.listen((process.env.PORT || 3000), function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+})
+
